6fc24b55225af3658e6aec8c34e73ba6af1df09f
Peter Cavagit config --global user.email <you@example.com>
2018-10-04 11:39:34 +0200

    add random

--- playground.js
+++ playground.js
@@ -1,2 +1,19 @@
-var myName = { firstname : "Peter", lastname : "It works!" };
-console.log(myName);
+function changeBackground() {
+
+    if (document.body.style.backgroundColor == "white") {
+        document.body.style.backgroundColor = "yellow";
+    } else {
+        document.body.style.backgroundColor = "white";
+    }    
+}
+
+
+function windowAlert() {
+    var text;
+    if (confirm("Press a button!")) {
+        text = "You pressed OK!";
+    } else {
+        text = "You pressed cancel!";
+    }
+    document.getElementById.alert1("alert");
+}
