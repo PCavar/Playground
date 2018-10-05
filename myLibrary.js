326c7bc7c35eb02007f6fb1076d9337fff54f292
Peter Cavagit config --global user.email <you@example.com>
2018-10-05 09:15:33 +0200

    initial command

--- myLibrary.js
+++ myLibrary.js
@@ -1,1 +1,28 @@
-console.log("hej");
+var myLibrary = function() {
+/**
+ * calculates the area of a rectangle based of it's width and height
+ * @param width {number}
+ * @param height {number}
+ * @returns the area of the rectangle
+ */
+}
+
+function rectangleArea(width, height) {
+    return width * height;
+}
+
+
+/**
+ * calculates the area of a triangle based on it's base and height
+ * @param base {number}
+ * @param base {number}
+ * @returns base and height of the triangle
+ */
+
+function triangleArea(base, height) {
+    return base * height / 2;
+}
+
+function circleArea(radius) {
+    return Math.PI * radius * radius;
+}
--- playground.js
+++ playground.js
@@ -69,3 +69,4 @@ function convertToHex(value) {
     }
 
 }
+
